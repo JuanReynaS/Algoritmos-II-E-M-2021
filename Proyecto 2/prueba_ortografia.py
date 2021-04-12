309e77b783badc61cbf435e7351546e56b540db0
JuanReynaS <55553459+JuanReynaS@users.noreply.github.com>
2021-04-12 00:32:07 -0400

    Update prueba_ortografia.py

--- Proyecto 2/prueba_ortografia.py
+++ Proyecto 2/prueba_ortografia.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 import time
 import ayudante_ortografico as ao
-from colorama import Cursor, init, Fore, Style, Back
+from colorama import init, Fore, Style, Back
 init()
+
 
 def clear():
     if os.name == "posix":
@@ -11,19 +15,21 @@ def clear():
     elif os.name == "ce" or os.name == "nt" or os.name == "dos":
         os.system("cls")
 
+
 def err_ao():
     print(Fore.RED + Back.WHITE + Style.BRIGHT 
           + "Todavía no ha sido creado un ayudante ortográfico"
           + Fore.RESET + Back.RESET)
     time.sleep(2)
-    clear()      
+    clear()
+
 
 def archivo_selecc():
     c = 0
     with os.scandir() as ficheros:
         ficheros = sorted([fichero.name for fichero in ficheros
                            if fichero.is_file()])
-    
+
     print("\n\nEn su directorio actual se encuentran los siguientes archivos:\n")
     for i in ficheros:
         print("{}: {}".format(c, i))
@@ -62,16 +68,16 @@ while True:
 
         elif N == 2:
             if help_o is None:
-                err_ao() 
+                err_ao()
             else:
                 clear()
-                while True:    
+                while True:
                     try:
                         lista_archivo = archivo_selecc()
                         print("00: Regresar a menu principal")
                         print("-1: Actualizar archivos de su directorio")
                         diccionario = int(input("Elija el diccionario que desea cargar "))
-                        
+
                         if diccionario == 00:
                             clear()
                             break
@@ -85,101 +91,100 @@ while True:
                             clear()
                         elif ((lista_archivo[diccionario]  not in guar_dicc
                                and 0 <= diccionario < len(lista_archivo))):
-
-                            if help_o.cargarDiccionario(lista_archivo[diccionario]) is True:
+                            cargar_dicc = help_o.cargarDiccionario(lista_archivo[diccionario])
+                            if cargar_dicc is True:
                                 guar_dicc.append(lista_archivo[diccionario])
                                 crear_dicc = True
-                                
+
                                 print(Fore.GREEN + Back.WHITE + Style.BRIGHT
                                       + "Se ha cargado un diccionario" + Fore.RESET
                                       + Back.RESET)
-                                
+
                                 od = int(input("¿Desea cargar otro diccionario? 1. Si 2. No "))
                                 if od == 1:
-                                    pass
+                                    clear()
                                 elif od == 2:
                                     clear()
                                     break
-                            else:
-                                
+                            elif cargar_dicc is False:
+
                                 print(Fore.RED + Back.WHITE + Style.BRIGHT
                                       + "Archivo seleccionado no es válido como diccionario"
                                       + Fore.RESET + Back.RESET)
-                                
+
                                 time.sleep(1.8)
-                                clear()                
-                    
+                                clear()
+                        else:
+
+                            print(Fore.RED + Back.WHITE + Style.BRIGHT
+                                  + "El diccionario elegido ya está cargado"
+                                  + Fore.RESET + Back.RESET)
+
+                            mostrar_dicc = int(input("Desea ver los diccionarios cargados 1. Si 2. No "))
+                            if mostrar_dicc == 1:
+                                print(guar_dicc)
+                                enter = input("Presione Enter para continuar...")
+                                clear()
+                            elif mostrar_dicc == 2:
+                                pass
+                            else:
+                                print(Fore.RED + Back.WHITE + Style.BRIGHT
+                                      + "Opción no válida" + Fore.RESET + Back.RESET)
+
+                                clear()
+
                     except ValueError:
 
                         print(Fore.RED + Back.WHITE + Style.BRIGHT
                               + "Opción no válida" + Fore.RESET + Back.RESET)
-        
+
                         time.sleep(1.2)
                         clear()
-
-
-                    else:
-                        
-                        print(Fore.RED + Back.WHITE + Style.BRIGHT
-                              + "El diccionario elegido ya está cargado"
-                              + Fore.RESET + Back.RESET)
-                        
-                        mostrar_dicc = int(input("Desea ver los diccionarios cargados 1. Si 2. No "))
-                        if mostrar_dicc == 1:
-                            print(guar_dicc)
-                            enter = input("Presione Enter para continuar...")
-                        elif mostrar_dicc == 2:
-                            pass
-                        else:
-                            print(Fore.RED + Back.WHITE + Style.BRIGHT
-                                  + "Opción no válida" + Fore.RESET + Back.RESET)
-
-                    clear()
         elif N == 3:
             if help_o is None:
-                err_ao() 
-            else:        
+                err_ao()
+            else:
                 if crear_dicc is True:
                     palabra = input("Ingrese la palabra que desea eliminar ")
                     borrar = help_o.borrarPalabra(palabra)
                     if borrar is True:
-                        
+
                         print(Fore.GREEN + Back.WHITE + Style.BRIGHT
                               + "Palabra eliminada: {}".format(palabra)
                               + Fore.RESET + Back.RESET)
-                        
+
                         time.sleep(1.8)
                         clear()
                     elif borrar is None:
-                        
+
                         print(Fore.RED + Back.WHITE + Style.BRIGHT 
                               + "Palabra no está en diccionario"
                               + Fore.RESET + Back.RESET)
-                        
+
                         time.sleep(1.8)
                         clear()
                     elif borrar is False:
-                        
+
                         print(Fore.RED + Back.WHITE + Style.BRIGHT
                               + "Palabra no es válida"
                               + Fore.RESET + Back.RESET)
-                        
+
                         time.sleep(1.8)
                         clear()
 
                 else:
-                    
+
                     print(Fore.RED + Back.WHITE + Style.BRIGHT 
                           + "Aún no ha cargado un diccionario"
                           + Fore.RESET + Back.RESET)
-                    
+
                     time.sleep(1.8)
                     clear()
 
         elif N == 4:
-            
+
             if help_o is None:
-                err_ao() 
+                err_ao()
             else:
                 clear()
                 if crear_dicc is False:
@@ -187,53 +192,54 @@ while True:
                           + "No se ha cargado diccionario"
                           + Fore.RESET + Back.RESET)
                     time.sleep(1.8)
-                    clear()      
-                else:    
-                    while True:    
-                        try:    
+                    clear()
+                else:
+                    while True:
+                        try:
                             lista_archivo = archivo_selecc()
                             print("00: Regresar a menu principal")
                             texto = int(input("Elija el texto que desea corregir "))
                             if texto == 00:
                                 clear()
                                 break
-                            elif lista_archivo[texto] in guar_dicc:
-                                
+                            if lista_archivo[texto] in guar_dicc:
+
                                 print(Fore.RED + Back.WHITE + Style.BRIGHT
                                       + "No puede corregir archivo cargado como diccionario"
                                       + Fore.RESET + Back.RESET)
                                 
                                 time.sleep(1.8)
                                 clear()
-                            else:    
+                            else:
                                 salida = help_o.corregirTexto(lista_archivo[texto])
-                                
+
                                 print(Fore.GREEN + Back.WHITE + Style.BRIGHT
                                       + "\nSe generó el archivo: {}".format(salida)
                                       + Fore.RESET + Back.RESET)
-                                
+
                                 time.sleep(3)
                                 clear()
+                        
                         except ValueError:
 
                             print(Fore.RED + Back.WHITE + Style.BRIGHT
                                   + "Opción no válida" + Fore.RESET + Back.RESET)
-        
+
                             time.sleep(1.2)
                             clear()
-        elif N == 5:      
+        elif N == 5:
             if help_o is None:
-                err_ao()       
+                err_ao()
             else:
                 if crear_dicc is False:
-                    
+
                     print(Fore.RED + Back.WHITE + Style.BRIGHT
                           + "No se ha cargado diccionario"
                           + Fore.RESET + Back.RESET)
-                    
+
                     time.sleep(1.8)
-                    clear()      
-                else:       
+                    clear()
+                else:
                     help_o.mostrar()
                     continuar = input("Presione enter para continuar...")
                     clear()
@@ -253,6 +259,6 @@ while True:
 
         print(Fore.RED + Back.WHITE + Style.BRIGHT
               + "Opción no válida" + Fore.RESET + Back.RESET)
-        
+
         time.sleep(1.2)
         clear()
